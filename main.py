a5bd2be0cbe2fe5e4619bebb6161bc9f3c8baddf
hz.oscar <hz.oscar.ist@gmail.com>
2025-04-05 23:24:23 +0200

    Refactor arithmetic_arranger function to improve error handling and formatting

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-def arithmetic_arranger(problems, show_answers=False):
-
-    return problems
-
-print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49", "99 + 1"])}')
-
 def do_operation(first_value,operator,second_value):
     if operator == "+":
         return str(int(first_value) + int(second_value))
@@ -17,33 +11,46 @@ def arithmetic_arranger(problems, show_answers=False):
     output_second_line_list = []
     dash_line = []
     output_answer_list = []
-    separator="    "
-
+    separator = "    "
+    if len(problems) > 5:
+        return "Error: Too many problems."
+    
     for problem in problems:
         problem = problem.split()
         first_line= problem[0]
         operator = problem[1]
-        second_line= problem[2]               
-        len_answer_line = max(len(first_line), len(second_line))+2
-        answer_line = "_" * len_answer_line
-        dash_line.append(answer_line)
+        if operator not in ["+", "-"]:
+            return "Error: Operator must be '+' or '-'."
+        second_line= problem[2]  
         
+        if len(first_line)>=5 or len(second_line)>=5:
+            return "Error: Numbers cannot be more than four digits."
+        
+        if not first_line.isdigit() or not second_line.isdigit():
+            return "Error: Numbers must only contain digits."
+                     
+        bigger_number = max(len(first_line), len(second_line))
+        difference = abs(len(first_line) - len(second_line))        
+        answer_line = "-" * (bigger_number + 2)
+        dash_line.append(answer_line)
+        get_answer = do_operation(first_line, operator, second_line)
+        spaces = bigger_number + 2 - len(get_answer)
+                
         if len(first_line)>len(second_line):
-            second_line = " " * (len(first_line) - len(second_line)) + second_line 
+            second_line = " " * (difference) + second_line 
             output_first_line_list.append("  " + first_line)
             output_second_line_list.append(operator +' '+ second_line) 
-            output_answer_list.append("  " + " " * (len(first_line) - len(second_line))+do_operation(first_line, operator, second_line))        
+            output_answer_list.append(" " * spaces + get_answer)        
             
         elif len(first_line)==len(second_line):
             output_first_line_list.append("  " +first_line)
             output_second_line_list.append(operator +' '+ second_line)
-            output_answer_list.append("  " + do_operation(first_line, operator, second_line))
+            output_answer_list.append(" " * spaces + get_answer)
         else:
-            first_line = " " * (len(second_line) - len(first_line)) + first_line
-            output_first_line_list.append("  " +first_line)
-            output_second_line_list.append(operator+' '+ second_line) 
-            output_answer_list.append( "  " +" " * (len(second_line) - len(first_line))+ do_operation(first_line, operator, second_line))
-
+            first_line = " " * (difference) + first_line
+            output_first_line_list.append("  " + first_line)
+            output_second_line_list.append(operator +' '+ second_line) 
+            output_answer_list.append(" " * spaces + get_answer)
 
     final_output = [separator.join(output_first_line_list), separator.join(output_second_line_list), separator.join(dash_line)]
     if show_answers:
@@ -52,5 +59,5 @@ def arithmetic_arranger(problems, show_answers=False):
     for i in final_output:
         print(i, end= '\n')
 
-arithmetic_arranger(problems, show_answers= True)
-#arithmetic_arranger(problems)
+    #return "\n".join(final_output)
+    
